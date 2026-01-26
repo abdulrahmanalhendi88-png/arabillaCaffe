5a64be902185c09a49f9c1427420465fde87d19b
abdulrahmanalhendi88-png <abdulrahmanalhendi88@gmail.com>
2026-01-26 13:21:35 +0300

    Update files

--- arabella/urls.py
+++ arabella/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("menu.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# يخدم الـ media حتى مع DEBUG=0 (مناسب لمشروع مينيو)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
