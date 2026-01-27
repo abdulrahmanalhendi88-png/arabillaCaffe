689f1a89863c0ea6554cdbd6f09361f56803eb65
abdulrahmanalhendi88-png <abdulrahmanalhendi88@gmail.com>
2026-01-27 15:10:57 +0300

    Serve media files on Render

--- arabella/urls.py
+++ arabella/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("menu.urls")),
-]
 
-# ✅ خدم الميديا دائماً (حتى لو DEBUG=False) طالما بدك تخدمها من نفس التطبيق
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # ✅ خدم ملفات الميديا دائماً من /var/data/media
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
