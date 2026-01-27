5dc41b3f74cb15bcbe031ec64dad44886759a018
abdulrahmanalhendi88-png <abdulrahmanalhendi88@gmail.com>
2026-01-27 14:58:26 +0300

    replace arabella-urls.py2

--- arabella/urls.py
+++ arabella/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +8,5 @@ urlpatterns = [
     path("", include("menu.urls")),
 ]
 
-# ✅ Render: خدم ملفات الميديا حتى مع DEBUG=False
+# ✅ خدم الميديا دائماً (حتى لو DEBUG=False) طالما بدك تخدمها من نفس التطبيق
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
